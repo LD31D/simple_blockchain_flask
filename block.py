de62ed76c452b284ee060265987ab018bd6d45e6
Artem <LD31D@github.com>
2019-12-26 21:58:20 +0200

    Add block.py

---
+++ block.py
@@ -0,0 +1,96 @@
+from hashlib import md5
+from json import dump, load
+from os import curdir, listdir, path, mkdir
+
+
+def get_hash(path_to_file):
+	text = open(path_to_file, 'rb').read()
+
+	return md5(text).hexdigest()
+
+
+def check_integrity():
+
+	result = []
+
+	blockchain_dir = curdir + '/blockchain/'
+
+	if path.exists(blockchain_dir) == False:
+		mkdir(blockchain_dir)
+
+	files = listdir(blockchain_dir)
+
+	if files == []:
+		return None
+
+	files = sorted([element for element in files if element.isdigit() if int(element) > 0], key=int)
+
+	for filename in files:
+
+		text = load(open(blockchain_dir + filename))
+
+		try:
+			file_hash = text['hash']
+
+		except KeyError:
+			return None
+
+		if str(file_hash).replace(" ", "") == "":
+			continue
+
+		prev_file = int(filename) - 1
+
+		actual_hash = get_hash(blockchain_dir + str(prev_file))
+
+		if file_hash == actual_hash:
+			res = "Ok"
+		else:
+			res = "Corrapted"
+
+		result.append(res)
+		
+	return result
+
+
+def write_block(name, amount, to_whom):
+
+	blockchain_dir = curdir + '/blockchain/'
+
+	if path.exists(blockchain_dir) == False:
+		mkdir(blockchain_dir)
+
+	files = listdir(blockchain_dir)
+
+	if files != []:
+		files = sorted([int(element) for element in files if element.isdigit() if int(element) > 0], key=int)
+
+		last_file = files[-1]
+
+		filename = str(last_file + 1)
+
+		path_to_file = blockchain_dir + str(last_file)
+
+		prev_hash = get_hash(path_to_file)
+
+	else:
+		filename = '1'
+		prev_hash=''
+
+	data = {
+		'from': name,
+		'amount': amount,
+		'to_whom': to_whom,
+		'hash': prev_hash
+	}
+
+	with open(blockchain_dir + filename, 'w') as file:
+		dump(data, file, indent=4, ensure_ascii=False)
+
+
+def main():
+	#write_block("sfa", 13, "fwfwef")
+	print(check_integrity())
+
+
+if __name__ == '__main__':
+	main()
